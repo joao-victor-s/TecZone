58ed7f6941902e20c7a3add81eeb86873d3249f1
João Victor Silva <joaovictorsilvaesantos@gmail.com>
2025-07-26 18:36:37 -0300

    add repository contracts and base repository structure

---
+++ TecZone.Repositorio/Repos/BaseRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TecZone.Dominio.Contracts;
+
+namespace TecZone.Repositorio.Repos
+{
+    public class BaseRepo : IBaseRepo
+    {
+    }
+}
---
+++ TecZone.Repositorio/Repos/ItemRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TecZone.Repositorio.Repos
+{
+    internal class ItemRepo
+    {
+    }
+}
